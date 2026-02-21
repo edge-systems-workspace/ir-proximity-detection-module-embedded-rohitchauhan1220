3205d445005afe60e1146efb7bdc245e63705d09
rohitchauhan1220 <rohit.chauhan_cs.aiml24@gla.ac.in>
2026-02-21 23:25:03 +0530

    Initialize Serial communication at 9600 baud rate

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,7 @@ int irState = 0;
 
 void setup() {
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
+    Serial.begin(9600);
     // TODO 4:
     // Configure IR pin as INPUT
 
